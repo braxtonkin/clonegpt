0ed519d9267bc48cf1fde9c5c4856ef09c75747e
braxtonkin <kinneybraxton@gmail.com>
2025-02-09 23:33:08 -0800

    further cleanup

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,14 +1,17 @@
 import Chat from "@/app/ui/Chat";
 import ChatInput from "@/app/ui/ChatInput";
 
-export default async function ChatPage({params:{id}}: {params: {id: string}}) {
-    return (
-      <div className = "flex flex-col justify-center h-[100%] p-10 overflow-hidden">
-        <div className="flex-1 overflow-y-scroll pt-10 md:pt-5"> 
-          <Chat id={id}/>
-           </div>
-        <ChatInput id={id} />
-      </div>
-    )
-  }
+export default async function ChatPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = await Promise.resolve(params.id);
+
+  return (
+    <div className = "flex flex-col justify-center h-[100%] p-10 overflow-hidden">
+      <div className="flex-1 overflow-y-scroll pt-10 md:pt-5"> 
+        <Chat id={id}/>
+         </div>
+      <ChatInput id={id} />
+    </div>
+  )
+}
   
--- src/app/ui/ChatRow.tsx
+++ src/app/ui/ChatRow.tsx
@@ -1,9 +1,6 @@
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation"
 
 export default function ChatRow({id}: {id: string}) {
-    const pathname = usePathname();
-    const router = useRouter();
     return (
       <Link href={`/chat/${id}`} className="flex gap-2 items-center justify-center px-2 py-1.5 bg-white/10 mb-2 hover:bg-white/10 rounded-md duration-md ease-in">
         <p className="md:inline-flex truncate flex-1 text-sm font-medium tracking-wide justify-center items-center"> Chat {id} </p>
